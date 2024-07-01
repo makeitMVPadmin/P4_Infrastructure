fc3979d665059d5745e0bee90f1eb6652faf2b0b
Jiachun Xiang <jcxiang1230@gmail.com>
2024-07-01 18:17:34 -0400

    try to fix complier problems with changing import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./Pages/HomePage/HomePage";
+import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 }
