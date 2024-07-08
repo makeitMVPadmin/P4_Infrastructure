0a424388d63a938f921e0f30fb84eb2a546c26a4
Soham Gaggenapally <55066921+sgaggen@users.noreply.github.com>
2024-07-08 16:26:11 -0400

    Fix readme styling and add react-router-dom dependency

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
 }
 
 export default App;
